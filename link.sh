50d51b0fc7103d833574a0d5df2f1fd14d49e379
Dylan Quenneville <dylan.quenneville@dylan.quenneville>
2020-09-08 15:13:07 -0700

    Fix directories with spaces

--- link.sh
+++ link.sh
@@ -11,7 +11,7 @@ DIR=$(dirname "$0")
 PARENT_PATH=$(pwd -P)
 CONFIG_DIR="${PARENT_PATH}/${DIR}"
 
-if [ -n $WSL_DISTRO_NAME ]
+if [ -n "$WSL_DISTRO_NAME" ]
 then
 VSCODE_SETTINGS_DIR="$HOME/.vscode-server/data/Machine"
 else
@@ -21,12 +21,12 @@ fi
 cd ~
 
 # config
-symlink $CONFIG_DIR/.bash_profile .bash_profile
-symlink $CONFIG_DIR/.vimrc .vimrc
-symlink $CONFIG_DIR/.zshrc .zshrc
+symlink "$CONFIG_DIR/.bash_profile" .bash_profile
+symlink "$CONFIG_DIR/.vimrc" .vimrc
+symlink "$CONFIG_DIR/.zshrc" .zshrc
 
 # vscode
-cd $VSCODE_SETTINGS_DIR
-symlink $CONFIG_DIR/vscode/settings.json settings.json
-symlink $CONFIG_DIR/vscode/keybindings.json keybindings.json
+cd "$VSCODE_SETTINGS_DIR"
+symlink "$CONFIG_DIR/vscode/settings.json" settings.json
+symlink "$CONFIG_DIR/vscode/keybindings.json" keybindings.json
 cd ~
